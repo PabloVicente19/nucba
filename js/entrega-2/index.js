0f9fdfbfd9fd6376cf1e043a007925c4325bf5f8
Pablo Vicente <pablovicente19@hotmail.com>
2022-10-13 19:44:52 -0300

    arrancando el segundo desafio

---
+++ js/entrega-2/index.js
@@ -0,0 +1,13 @@
+/*
+Vamos a utilizar el mismo array de objetos "Pizzas🍕" del desafío general anterior. 
+
+👉 Crear un archivo HTML con un input de tipo number, un botón y un contenedor en el cual renderizar el resultado de la búsqueda. 
+
+👉 El desafío será, al tocar el botón, capturar el valor ingresado en el input.
+👉 Renderizar en el contenedor un h2 con el nombre y en un h3 con el precio de la pizza cuyo id coincida con el numero ingresado en el input. 
+
+🚨 Si el número ingresado no coincide con ningún id, renderizar (no sirve un alert) un mensaje de error en el contenedor. 
+🚨 Si no se ingresa un número, renderizar (no sirve un alert) un mensaje de error diferente en el contenedor. 
+
+🆙 Entregar el link de Github , en el cual debe estar linkeado el deploy del Vercel de su aplicación (mediante Github nosotros deberíamos poder ver el Vercel vinculado a su repositorio). 
+*/
